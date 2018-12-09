c57273fc0dbb2622006509c745cfbc90e2237b35
Nicolas Peltier <peltier.nicolas@gmail.com>
2018-12-09 21:41:30 +0100

    start post layout

--- footer.php
+++ footer.php
@@ -14,19 +14,9 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'potager' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'potager' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'potager' ), 'potager', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		<p>
+			© potager liberté   /  Mentions légales
+		</p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -20,7 +20,7 @@
 
 		if ( 'post' === get_post_type() ) :
 			?>
-			<div class="entry-meta">
+			<!--div class="entry-meta">
 				<?php
 				potager_posted_on();
 				potager_posted_by();
