05cfb4f6bdb1926a32b917cb978067422c076e4c
Nicolas Peltier <peltier.nicolas@gmail.com>
2019-01-14 21:34:22 +0100

    fixes #2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z $key ]
 then
-        read -p "Enter key location string: " key
+        read -p "Enter key location string (to generate it, please look at https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/) " key
 fi
 if [ -z $target ]
 then
