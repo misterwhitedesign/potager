90eb62a43e3a409e4acffdd08b9b3a247d64084b
Nicolas Peltier <peltier.nicolas@gmail.com>
2019-01-31 05:16:21 -0800

    fix titre projet

--- template-parts/content-projet.php
+++ template-parts/content-projet.php
@@ -9,16 +9,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" style="background-image=url('<?php echo $src ?>')" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-   ?>
-	</header><!-- .entry-header -->
-
+	<?php
+		the_title( '<h1 class="entry-title">', '</h1>' );
+	?>
 	<div class="entry-content">
 		<?php
 		the_content( sprintf(
