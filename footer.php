f8fb25283432a17b3bd604439ef510efe83d8662
Nicolas Peltier <peltier.nicolas@gmail.com>
2019-01-31 01:41:30 -0800

    another set

--- footer.php
+++ footer.php
@@ -29,8 +29,37 @@
 			endwhile;
 			?>
 		</section><?php
+	elseif ( $post->post_type == 'projet'):
+		//Get the images ids from the post_metadata
+ 		$images = acf_photo_gallery('gallerie', $post->ID);
+ 		//Check if return array has anything in it
+ 		if( count($images) ):
+ 			?>
+ 			<section class="gallerie-projet">
+ 			<?php
+ 				$index = 0;
+ 				//Cool, we got some data so now let's loop over it
+ 				foreach($images as $image):
+ 						$id = $image['id']; // The attachment id of the media
+ 						$title = $image['title']; //The title
+ 						$caption= $image['caption']; //The caption
+ 						$full_image_url= $image['full_image_url']; //Full size image url
+ 						$full_image_url = acf_photo_gallery_resize_image($full_image_url, 262, 160); //Resized size to 262px width by 160px height image url
+ 						$thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
+ 						$url= $image['url']; //Goto any link when clicked
+ 						$target= $image['target']; //Open normal or new tab
+ 						$alt = get_field('photo_gallery_alt', $id); //Get the alt which is a extra field (See below how to add extra fields)
+ 						$class = get_field('photo_gallery_class', $id); //Get the class which is a extra field (See below how to add extra fields)
+ 						$figure_class = ($index++ % 2 == 0 ? "droite" : "gauche");?>
+ 		<?php if( !empty($url) ){ ?><a href="<?php echo $url; ?>" <?php echo ($target == 'true' )? 'target="_blank"': ''; ?>><?php } ?>
+ 				<figure class="<?php echo $figure_class; ?>">
+ 					<img src="<?php echo $full_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
+ 				</figure>
+ 		<?php if( !empty($url) ){ ?></a><?php } ?>
+ <?php endforeach; ?>
+ 	</section>
+<?php endif; endif;?>
 
-	endif;?>
 	<footer id="colophon" class="site-footer">
 		<p>
 			© potager liberté   /  Mentions légales
--- template-parts/content-projet.php
+++ template-parts/content-projet.php
@@ -16,15 +16,7 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<!--div class="entry-meta">
-				<?php
-				potager_posted_on();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+   ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -40,36 +32,5 @@
 				)
 			),
 			get_the_title()
-		) );
-		//Get the images ids from the post_metadata
-		$images = acf_photo_gallery('gallerie', $post->ID);
-		//Check if return array has anything in it
-		if( count($images) ):
-			?>
-			</div><!-- .entry-content -->
-			<section class="gallerie-projet">
-			<?php
-				$index = 0;
-				//Cool, we got some data so now let's loop over it
-				foreach($images as $image):
-						$id = $image['id']; // The attachment id of the media
-						$title = $image['title']; //The title
-						$caption= $image['caption']; //The caption
-						$full_image_url= $image['full_image_url']; //Full size image url
-						$full_image_url = acf_photo_gallery_resize_image($full_image_url, 262, 160); //Resized size to 262px width by 160px height image url
-						$thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
-						$url= $image['url']; //Goto any link when clicked
-						$target= $image['target']; //Open normal or new tab
-						$alt = get_field('photo_gallery_alt', $id); //Get the alt which is a extra field (See below how to add extra fields)
-						$class = get_field('photo_gallery_class', $id); //Get the class which is a extra field (See below how to add extra fields)
-						$figure_class = ($index++ % 2 == 0 ? "droite" : "gauche");
-?>
-		<?php if( !empty($url) ){ ?><a href="<?php echo $url; ?>" <?php echo ($target == 'true' )? 'target="_blank"': ''; ?>><?php } ?>
-				<figure class="<?php echo $figure_class; ?>">
-					<img src="<?php echo $full_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
-				</figure>
-		<?php if( !empty($url) ){ ?></a><?php } ?>
-<?php endforeach; endif;
-		?>
-	</section>
+		) );?>
 </article><!-- #post-<?php the_ID(); ?> -->
