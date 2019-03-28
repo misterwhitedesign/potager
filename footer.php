061e8726b359b162c1d3f8d28184835c2b7025d1
Nicolas Peltier <peltier.nicolas@gmail.com>
2019-03-28 22:50:17 +0100

    ajustement tailles

--- footer.php
+++ footer.php
@@ -76,15 +76,15 @@
 <?php
 $animaux = new WP_Query(array('post_type'=>'animal', 'post_status'=>'publish', 'posts_per_page'=>-1));
 $index = 0;
-$possible_direction = ['go-right','go-left','go-down','go-up','bounce','riseturn'];
-$fixed_direction = array('martin'=>'go-down','puce'=>'bounce','hirondelle'=>'riseturn');
+$possible_direction = ['go-right','go-left','go-down','go-up','bounce','riseturn','bumpturn'];
+$fixed_direction = array('martin'=>'go-down','puce'=>'bounce','hirondelle'=>'riseturn','sifilet'=>'bumpturn');
 while ( $animaux->have_posts() ) :
 	$animaux->the_post();
 	$image = get_field("image",$id);
 	$animation = get_field("animation",$id);
 	$required_direction = $fixed_direction[$animation];
   $direction =  $required_direction != NULL ? $required_direction : $possible_direction[rand(0,4)];
-	$style = in_array($direction, ['go-right','go-left']) ? 'top: ' : 'left: ';
+	$style = in_array($direction, ['go-right','go-left','bumpturn']) ? 'top: ' : 'left: ';
 	$style = $style.rand(5,95).'%;';
 	$style = $style.'animation-delay: '.rand(0,5).'s';
 	$taille = get_field("taille",$id);
